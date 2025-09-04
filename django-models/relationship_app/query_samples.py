3880d3ab0876c9780f84f248c15fdabd8c49867b
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2025-09-04 10:53:05 +0100

    Fix queries with Django ORM best practices

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,17 +1,13 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-#query all books by specific author
+# Query all books by a specific author
 def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    books = Book.objects.filter(author=author)
-    return books
+    return Book.objects.filter(author__name=author_name)
 
-#List all books in a Library
+# List all books in a library
 def get_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
+    return Book.objects.filter(library__name=library_name)
 
-#Retrieve the librarian for a library
+# Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library__name=library_name)
