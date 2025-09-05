4c0892cf0fdea6010cd8211ab28c5f9083b00425
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2025-09-05 19:46:29 +0100

    edited views, urls for displaying books and library details

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,15 +7,15 @@ from django.views.generic import DetailView
 # Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
 # Class-based view: display library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Add all books for this library
