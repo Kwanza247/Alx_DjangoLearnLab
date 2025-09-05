be75c4e7d2e238acb2c87028b9ea705dfeb815f8
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2025-09-05 01:15:44 +0100

    Fixed query_samples formatting and queries for checker

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,16 +1,13 @@
 from relationship_app.models import Author, Book, Library, Librarian
-# List all books in a library
-
-def get_books_in_library(library_name):
-    return Library.objects.get(name=library_name).books.all()
-
 
 # Query all books by a specific author
 def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return Book.objects.filter(author=author)
+    return Book.objects.filter(author__name=author_name)
+
+# List all books in a library
+def get_books_in_library(library_name):
+    return Library.objects.get(name=library_name).books.all()
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     return Library.objects.get(name=library_name).librarian
-
