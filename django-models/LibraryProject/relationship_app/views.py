96ebc5fc1eb6503f48146546c5ac09c42a8c59ee
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2025-09-05 19:28:28 +0100

    Added views, urls, templates for books and library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import DetailView
 
 # Create your views here.
+
+# Function-based view: list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "list_books.html", {"books": books})
+
+
+# Class-based view: display library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add all books for this library
+        context["books"] = self.object.books.all()
+        return context
