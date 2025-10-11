af81991ee1f1cb6299cdb990f81fb39b8047dc1b
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2025-10-11 17:32:25 +0100

    Prepare app for Render deployment

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7l-d2u-)x52&wbdr-0lo@e@h-9dt59-lhv!fr*pfdr#g@$jb9f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ibrahim-sma.herokuapp.com']
+ALLOWED_HOSTS = ['ibrahim-sma.onrender.com']
 
 
 # Application definition
