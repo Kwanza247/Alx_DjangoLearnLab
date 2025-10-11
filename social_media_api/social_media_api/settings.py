9b9ae9b4efae8becf4a69d3eba672426a639513a
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2025-10-11 21:57:28 +0100

    updated DEBUG for prod

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+DEBUG = False
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
